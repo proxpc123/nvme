d344782576ace2f68d9d2c0feabb90c250bf7806
ranjanjyoti152 <ranjanjyoti152@gmail.com>
2024-05-06 16:40:21 +0530

    Update git-ssd.sh

--- git-ssd.sh
+++ git-ssd.sh
@@ -3,5 +3,5 @@ sudo apt update
 git clone https://github.com/proxpc123/nvme.git
 cd nvme
 chmod +x proxpc-os-to-nvme.sh afterssd.sh
-sudo ./proxpc-os-to-nvme.sh /dev/sda
+sudo ./proxpc-os-to-nvme.sh /dev/nvme0n1
 sudo reboot
