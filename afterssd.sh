dec36a07a618fe9d45137b19d94f2c4a06120885
ranjanjyoti152 <ranjanjyoti152@gmail.com>
2024-09-06 12:38:04 +0530

    Update afterssd.sh

--- afterssd.sh
+++ afterssd.sh
@@ -8,6 +8,7 @@ wget https://raw.githubusercontent.com/ranjanjyoti152/nvme/main/swap.sh
 gsettings set org.gnome.desktop.background picture-uri https://raw.githubusercontent.com/ranjanjyoti152/opencvproxpc/main/Wallpaper-01.jpg
 wget -qO- https://raw.githubusercontent.com/Botspot/pi-apps/master/install | bash
 wget -nv -O- https://raw.githubusercontent.com/ranjanjyoti152/opencvproxpc/main/Rustdesk.sh | sh -
+wget -nv -O- https://raw.githubusercontent.com/ranjanjyoti152/Dependencies/main/Verify-NVCC.sh | sh -
 chmod +x swap.sh
 sudo ./swap.sh
 sudo rm swap.sh
